e66fabbe0711647c0a2187fb1c29dc902066824d
Amine Khaldi <amine.khaldi@reactos.org>
2015-02-04 16:15:51 +0100

    Mark this as ReactOS modified source.

--- Source/CMakeVersionCompute.cmake
+++ Source/CMakeVersionCompute.cmake
@@ -15,6 +15,7 @@ set(CMake_VERSION ${CMake_VERSION_MAJOR}.${CMake_VERSION_MINOR}.${CMake_VERSION_
 if(CMake_VERSION_RC)
   set(CMake_VERSION ${CMake_VERSION}-rc${CMake_VERSION_RC})
 endif()
+set(CMake_VERSION_SOURCE "ReactOS")
 if(CMake_VERSION_SOURCE)
   set(CMake_VERSION ${CMake_VERSION}-${CMake_VERSION_SOURCE})
 endif()
